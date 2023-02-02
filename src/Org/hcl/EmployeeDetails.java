321ad001034ca8a3781176d1c1ee33fafca545a2
GOpinathKgn <gopikaruppasamy@gmail.com>
2023-02-02 12:24:47 +0530

    Java

---
+++ src/Org/hcl/EmployeeDetails.java
@@ -0,0 +1,20 @@
+package Org.hcl;
+
+public class EmployeeDetails {
+private void employeeName() {
+	
+	System.out.println("Employee Name: Ram");
+}
+private void employeeId() {
+	
+	System.out.println("Employee ID: 2880");
+}
+	public static void main(String[] args) {
+		
+		EmployeeDetails e = new EmployeeDetails();
+		e.employeeName();
+		e.employeeId();
+	}
+	
+	
+}
