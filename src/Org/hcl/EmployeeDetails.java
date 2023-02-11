328dc96e01669b51eb9de5ba19e929cb6e684c9e
GopinathKgn <124070468+GopinathKgn@users.noreply.github.com>
2023-02-11 15:20:03 +0530

    added company name
    In method block added company name

--- src/Org/hcl/EmployeeDetails.java
+++ src/Org/hcl/EmployeeDetails.java
@@ -4,6 +4,7 @@ public class EmployeeDetails {
 private void employeeName() {
 	
 	System.out.println("Employee Name: Ram");
+	Systee.out.println("Company Name : HCL");
 }
 private void employeeId() {
 	
